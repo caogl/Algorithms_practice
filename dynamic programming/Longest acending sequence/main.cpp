529294638c3e41046936cdd352355609dea3fe4a
caogl <caogl@umich.edu>
2014-02-02 19:26:28 -0500

    Update main.cpp

--- dynamic programming/Longest acending sequence/main.cpp
+++ dynamic programming/Longest acending sequence/main.cpp
@@ -1,12 +1,13 @@
 #include<iostream>
+#include<algorithm>
 #include<vector>
 using namespace std;
 
 int LAS(int *l, int n);
 int main()
 {
-	int l[]={5,9,3,4,8,6,7,10,1,2,9,11,12,14,13,-10,-9,-8,-7,-6,-5,15,-4,-3,-1,0};
-	cout<<"The longest sub-acending sequnence is: "<<LAS(l, 26)<<endl;
+	int l[]={5,9,3,4,8,6,7,10,1,2,9,11,12,14,13,-10,-9,-8,-7,-6,15,-5,-4,-3,-1,0,-11};
+	cout<<"The longest sub-acending sequnence is: "<<LAS(l, 27)<<endl;
 	system("PAUSE");
 	return 0;
 }
@@ -21,5 +22,7 @@ int LAS(int *l, int n)
 			if(l[j]<l[i] && longest[j]+1>longest[i])
 				longest[i]=longest[j]+1;
 	}
-	return longest[n-1];
+	// Notice unlike the sum coin problem, here we must return the largest element in the result vector
+	// not the last one.
+	return *max_element(longest.begin(), longest.end()); 
 }
