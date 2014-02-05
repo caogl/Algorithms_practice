1663450bbbf6caeaca7617e9c22a16ec246bf6fa
caogl <caogl@umich.edu>
2014-02-04 21:27:01 -0500

    Update main.cpp

--- sorting and searching/11.5/main.cpp
+++ sorting and searching/11.5/main.cpp
@@ -1,26 +1,33 @@
-#include <iostream>
+##include <iostream>
 #include<string>
 using namespace std;
 
-int search(string s[], int low, int high, string x)
+int search(string s[], int left, int right, string x)
 {
-	int mid=(low+high)/2;
+
+	int mid=(left+right)/2;
 	if(s[mid]==x)
 		return mid;
-	int t=mid;
-	while(s[t]=="")
-		t++;
-	if(t>high)
-		return search(s, low, mid-1, x);
-	else 
+	if(s[mid]=="")
 	{
-		if(s[t]>x)
-			return search(s, low, mid-1, x);
-		else
-			return search(s, mid+1, high, x);
+		int t=mid;
+		while(s[t]=="")
+		{
+			t++;
+			if(t>right)
+				return search(s, left, mid-1, x);
+		}
+		mid=t;
+		if(s[mid]==x)
+			return mid;
 	}
+	if(x>s[mid])
+		return search(s, mid+1, right, x);
+	else
+		return search(s, left, mid-1, x);
 }
-int main(){
+int main()
+{
     string s[13] = {
         "at", "", "", "", "ball", "", "", "car", "", "", "dad", "", ""
     };
