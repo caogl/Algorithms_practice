9189ecf622e3f17b6b79670fcb80c895b12fca28
caogl <caogl@umich.edu>
2014-02-28 16:33:06 -0500

    update

--- recursion/Recursion-permutation, combination/main.cpp
+++ recursion/Recursion-permutation, combination/main.cpp
@@ -4,114 +4,71 @@
 #include<cctype>
 using namespace std;
 
-void genePerm(const vector<char>& in, vector<char>& out, vector<bool>& used);
-void geneComb(const vector<char>& in, vector<char>& out, int startPos);
-// generate permutation for upper and lower cases
-void genePermUL(const vector<char>& in, vector<char>& out, vector<bool>& used); 
-void printUL(vector<char>& in);
-void printUL1(vector<char>& out, const vector<vector<char> > in);
+vector<string> genePerm(const string str);
+void genePerm(vector<string>& result, string str, string& out, vector<bool>& used);
+vector<string> geneComb(const string str);
+void geneComb(vector<string>& result, string str, string& out, int startPos);
 
 int main()
 {
 	string inChar="abc";
-	vector<char> in;
-	for(unsigned int i=0; i<inChar.length(); i++)
-		in.push_back(inChar[i]);
-	vector<bool> used;
-	used.assign(in.size(), false);
-	vector<char> out;
-	genePermUL(in, out, used);
-	out.clear();
-	geneComb(in, out, 0);
+	vector<string> result=genePerm(inChar);
+	for(unsigned int i=0; i<result.size(); i++)
+		cout<<result[i]<<endl;
+	result=geneComb(inChar);
+	for(unsigned int i=0; i<result.size(); i++)
+		cout<<result[i]<<endl;
 	system("PAUSE");
 	return 0;
 }
 
-void geneComb(const vector<char>& in, vector<char>& out, int startPos)
+vector<string> geneComb(const string str)
 {
-	if(startPos==in.size())
+	vector<string> result;
+	string out;
+	int startPos=0;
+	geneComb(result, str, out, startPos);
+	return result;
+}
+
+void geneComb(vector<string>& result, string str, string& out, int startPos)
+{
+	if(startPos==str.size())
 		return;
-	for(int i=startPos; i<in.size(); i++)
+	for(int i=startPos; i<str.size(); i++)
 	{
-		out.push_back(in[i]);
-		for(unsigned j=0; j<out.size(); j++)
-			cout<<out[j]<<" ";
-		cout<<endl;
-		geneComb(in, out, i+1);
-		out.pop_back();
+		out+=str[i];
+		result.push_back(out);
+		geneComb(result, str, out, i+1);
+		out=out.substr(0, out.size()-1);
 	}
 }
-void genePerm(const vector<char>& in, vector<char>& out, vector<bool>& used)
+
+vector<string> genePerm(const string str)
 {
-	if(out.size()==in.size())
+	vector<string> result;
+	vector<bool> used(str.size(), false);
+	string out;
+	genePerm(result, str, out, used);
+	return result;
+}
+void genePerm(vector<string>& result, string str, string& out, vector<bool>& used)
+{
+	if(str.size()==out.size())
 	{
-		for(unsigned int i=0; i<out.size(); i++)
-			cout<<out[i]<<" ";
-		cout<<endl;
+		result.push_back(out);
 		return;
 	}
-	for(unsigned int i=0; i<in.size(); i++)
+	for(unsigned int i=0; i<str.size(); i++)
 	{
 		if(used[i]){}
 		else
 		{
+			out+=str[i];
 			used[i]=true;
-			out.push_back(in[i]);
-			genePerm(in, out, used);
-			out.pop_back();
+			genePerm(result, str, out, used);
 			used[i]=false;
+			out=out.substr(0, out.size()-1);
 		}
-	}
-}
-
-void genePermUL(const vector<char>& in, vector<char>& out, vector<bool>& used)
-{
-	if(out.size()==in.size())
-	{
-		printUL(out);
-		return;
-	}
-	for(unsigned int i=0; i<in.size(); i++)
-	{
-		if(used[i]){}
-		else
-		{
-			used[i]=true;
-			out.push_back(in[i]);
-			genePermUL(in, out, used);
-			out.pop_back();
-			used[i]=false;
-		}
-	}
-}
-
-void printUL(vector<char>& in)
-{
-	vector<char> out;
-	vector<char> tmp(2);
-	vector<vector<char> > index(in.size(), tmp); 
-	// the elements in the vector are the pair for upper and lower case of the original char
-	for(unsigned int i=0; i<in.size(); i++)
-	{
-		index[i][0]=tolower(in[i]);
-		index[i][1]=toupper(in[i]);
-	}
-	printUL1(out, index);
-}
-
-void printUL1(vector<char>& out, const vector<vector<char> > in)
-{
-	if(out.size()==in.size())
-	{
-		for(unsigned int i=0; i<out.size(); i++)
-			cout<<out[i]<<" ";
-		cout<<endl;
-		return;
-	}
-	for(unsigned int i=0; i<in[out.size()].size(); i++)
-	{
-		out.push_back(in[out.size()][i]);
-		printUL1(out, in);
-		out.pop_back();
 	}
 }
