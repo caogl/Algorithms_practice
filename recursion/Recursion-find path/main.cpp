865e504d8811c090aa2073b1ad56e10a68902035
caogl <caogl@umich.edu>
2014-02-28 21:41:52 -0500

    Update main.cpp

--- recursion/Recursion-find path/main.cpp
+++ recursion/Recursion-find path/main.cpp
@@ -2,37 +2,41 @@
 #include<utility>
 using namespace std;
 
-void findPath(int graph[][4], pair<int, int> *path, int rowNum, int colNum, int row, int col, int& count);
+int findPath(int graph[][4], int row, int col);
+void findPath(int graph[][4], int rowNum, int colNum, int row, int col, int& count);
 
 int main()
 {
 	int graph[][4]={{1,1,1,0}, {0,1,1,1}, {1,1,1,1}};
-	pair<int, int> path[7]; 
-	int count=0;
-	findPath(graph, path, 3, 4, 0, 0, count);
-	cout<<"The number of possible path is: "<<count<<endl;
+	for(int i=0; i<3; i++)
+	{
+		for(int j=0; j<4; j++)
+			cout<<graph[i][j]<<" ";
+		cout<<endl;
+	}
+	cout<<"The number of possible path is: "<<findPath(graph, 3, 4)<<endl;
 	system("PAUSE");
 	return 0;
 }
 
-void findPath(int graph[][4], pair<int, int> *path, int rowNum, int colNum, int row, int col, int& count)
+int findPath(int graph[][4], int row, int col)
 {
-	//basecase one
-	if(graph[row][col]==0 || row==rowNum || col==colNum) // Notice the attainable inf of row and col should be rowNum-1 and colNum-1!!
-		return;
-	path[row+col]=make_pair(row, col);
-	//basecase two
-	if((row+col)==(rowNum+colNum-2))
-	{
-		count++;
-		for(int i=0 ;i<=(rowNum+colNum-2); i++)
-			cout<<"("<<path[i].first<<","<<path[i].second<<") ";
-		cout<<endl;
-	}
-	else
-	{
-		findPath(graph, path, rowNum, colNum, row+1, col, count);
-		findPath(graph, path, rowNum, colNum, row, col+1, count);
-	}
+	int rowNum=0, colNum=0, count=0;
+	findPath(graph, rowNum, colNum, row, col, count);
+	return count;
 }
 
+void findPath(int graph[][4], int rowNum, int colNum, int row, int col, int& count)
+{
+	if(rowNum==row || colNum==col)
+		return;
+	if(rowNum==row-1 && colNum==col-1)
+	{
+		count++;
+		return;
+	}
+	if(graph[rowNum+1][colNum])
+		findPath(graph, rowNum+1, colNum, row, col, count);
+	if(graph[rowNum][colNum+1])
+		findPath(graph, rowNum, colNum+1, row, col, count);
+}
