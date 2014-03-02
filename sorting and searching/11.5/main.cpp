f99b711642df204b65876ecdbd71eea5d0298044
caogl <caogl@umich.edu>
2014-03-02 14:48:26 -0800

    Update main.cpp

--- sorting and searching/11.5/main.cpp
+++ sorting and searching/11.5/main.cpp
@@ -6,7 +6,6 @@ int search(string s[], int left, int right, string x)
 {
 	if(left>right)
 	{
-		//cout<<"haha"<<endl;
 		return -1;
 	}
 	int mid=(left+right)/2;
@@ -25,19 +24,18 @@ int search(string s[], int left, int right, string x)
 	}
 	if(s[mid]==x)
 	{
-		//cout<<mid<<endl;
 		return mid;
 	}
 	else if(s[mid]>x)
-		search(s, left, mid-1, x);
+		return search(s, left, mid-1, x);
 	else
-		search(s, mid+1, right, x);
+		return search(s, mid+1, right, x);
 }
 
 int main()
 {
     string s[27] = {
-        "at", "", "", "", "ball", "", "", "car", "", "", "dad", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "",
+        "at", "", "", "", "ball", "", "", "car", "", "", "dad", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", ""
     };
     cout<<search(s, 0, 26, "ball")<<endl;
 	cout<<search(s, 0, 26, "car")<<endl;
