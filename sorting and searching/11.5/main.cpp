0382cf142585bb72979f8f5a6543f36af8449031
caogl <caogl@umich.edu>
2014-03-02 14:35:49 -0800

    update

--- sorting and searching/11.5/main.cpp
+++ sorting and searching/11.5/main.cpp
@@ -4,34 +4,45 @@ using namespace std;
 
 int search(string s[], int left, int right, string x)
 {
-
+	if(left>right)
+	{
+		//cout<<"haha"<<endl;
+		return -1;
+	}
 	int mid=(left+right)/2;
-	if(s[mid]==x)
-		return mid;
 	if(s[mid]=="")
 	{
 		int t=mid;
-		while(s[t]=="")
+		while(t<right && s[++t]==""){}
+		if(t==right)
 		{
-			t++;
-			if(t>right)
-				return search(s, left, mid-1, x);
+			return search(s, left, mid-1, x);
 		}
-		mid=t;
-		if(s[mid]==x)
-			return mid;
+		else
+		{
+			mid=t;
+		}
 	}
-	if(x>s[mid])
-		return search(s, mid+1, right, x);
+	if(s[mid]==x)
+	{
+		//cout<<mid<<endl;
+		return mid;
+	}
+	else if(s[mid]>x)
+		search(s, left, mid-1, x);
 	else
-		return search(s, left, mid-1, x);
+		search(s, mid+1, right, x);
 }
+
 int main()
 {
-    string s[13] = {
-        "at", "", "", "", "ball", "", "", "car", "", "", "dad", "", ""
+    string s[27] = {
+        "at", "", "", "", "ball", "", "", "car", "", "", "dad", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "",
     };
-    cout<<search(s, 0, 12, "ball")<<endl;
+    cout<<search(s, 0, 26, "ball")<<endl;
+	cout<<search(s, 0, 26, "car")<<endl;
+	cout<<search(s, 0, 26, "dad")<<endl;
+	cout<<search(s, 0, 26, "chunlei")<<endl;
 	system("PAUSE");
     return 0;
 }
