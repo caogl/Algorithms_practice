7dabd9a68254d2534f13e56c39b9d5bfb2c1b6e5
caogl <caogl@umich.edu>
2014-03-03 03:22:55 -0500

    Update main.cpp

--- list/Delete node from list/main.cpp
+++ list/Delete node from list/main.cpp
@@ -10,19 +10,34 @@ struct node
 node* init(int a[], int n);
 void remove(node *c);
 void printList(node* head);
+//void removeLast(node* c);
 
 int main()
 {
 	int a[]={0,1,2,3,4,5,6,7,8,9};
 	node* testHead=init(a, 10);
 	printList(testHead);
-	cout<<endl;
-	int nth=4;
+	int nth=8;
 	node *c=testHead;
 	for(int i=0; i<nth; i++)
 	{
 		c=c->next;
 	}
+	remove(c);
+	printList(testHead);
+
+	// if try to delete the last node in the list by using the removeLast function, 
+	// the result turns to be not application
+	int b[]={0,1,2,3,4,5,6,7,8,9};
+	testHead=init(b, 10);
+	printList(testHead);
+	nth=9;
+	node *d=testHead;
+	for(int i=0; i<nth; i++)
+	{
+		d=d->next;
+	}
+	//removeLast(d);
 	remove(c);
 	printList(testHead);
 	system("PAUSE");
@@ -50,13 +65,26 @@ node* init(int a[], int n)
 	return head;
 }
 
-void remove(node *c)
+/*
+if remove the last node like this, when print the list, the last node will still be printed
+with a wield value, THE REASON IS THAT THIS NEXT TO LAST NODE STILL POINTS TO THE MEMORY ADDRESS 
+THE HEAP, IT DOES NOT KNOW THAT THIS MEMORY IS DEALLOCATED !!!! --------DELETING THE LAST NODE IS
+IMPOSSIBLE AND NEED TO BE EXPLAINED
+void removeLast(node* c)
+{
+	delete c;
+	c=NULL;
+}
+*/
+
+void remove(node* c)
 {
 	if(c==NULL || c->next==NULL)
 		return;
-	node *d=c->next;
+	// whenever want to access next->, need to test whether it is NULL beforehand
+	c->data=c->next->data; 
+	node* d=c->next;
 	c->next=d->next;
-	c->data=d->data;
 	delete d;
 }
 
@@ -67,4 +95,5 @@ void printList(node* head)
 		cout<<head->data<<" ";
 		head=head->next;
 	}
+	cout<<endl;
 }
