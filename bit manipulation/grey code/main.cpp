26a09ffeee0d577c8d9abdac1adb4db713b7f16b
caogl <caogl@umich.edu>
2014-08-26 12:16:38 -0400

    update

--- bit manipulation/grey code/main.cpp
+++ bit manipulation/grey code/main.cpp
@@ -25,7 +25,6 @@ vector<int> grayCode(int n)
 	result.push_back(0);
 	for(int i=0; i<n; i++) // iteratively, generate vector<int> for length=1,2, 3...n
 	{
-		cout<<"haha"<<endl;
 		int highestBit=(1<<i);
 		for(int j=result.size()-1; j>=0; j--)
 			result.push_back(highestBit+result[j]);
