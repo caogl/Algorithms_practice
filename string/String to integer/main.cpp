b18ffcf4756f3ceb602ff57791c2ed35d8ca62f1
caogl <caogl@umich.edu>
2014-08-29 18:56:03 -0400

    update

---
+++ string/String to integer/main.cpp
@@ -0,0 +1,63 @@
+#include<iostream>
+#include<cstring>
+#include<climits>
+using namespace std;
+
+int atoi1(const char* str);
+
+int main()
+{
+	char a[]="6784";
+	char b[]="   -82613 213";
+	char c[]=" 25436712%$#2131";
+	char d[]="-2147483647";
+	char e[]=" - 123";
+	char f[]="++12";
+	char g[]="2147483649";
+	char h[]="+-213"; // easy to mistake!
+	cout<<atoi1(a)<<endl;
+	cout<<atoi1(b)<<endl;
+	cout<<atoi1(c)<<endl;
+	cout<<atoi1(d)<<endl;
+	cout<<atoi1(e)<<endl;
+	cout<<atoi1(f)<<endl;
+	cout<<atoi1(g)<<endl;
+	cout<<atoi1(h)<<endl;
+	return 0;
+}
+
+int atoi1(const char* str)
+{
+	int num=0; 
+	int sign=1;
+	int len=strlen(str);
+	int i=0;
+	while(str[i]==' ')
+	{
+		i++;
+		if(i==len)
+			return 0;
+	}
+	if(str[i]=='+')
+		i++;
+	else if(str[i]=='-')
+	{
+		i++;
+		sign=-1;
+	}
+	while(i<len)
+	{
+		if(str[i]==' ' || str[i]<'0' || str[i]>'9')
+			break;
+		if(INT_MAX/10<num || (INT_MAX/10==num && INT_MAX%10<(str[i]-'0')))
+		{
+			if(sign==1)
+				return INT_MAX;
+			else
+				return INT_MIN;
+		}
+		num=num*10+str[i]-'0';
+		i++;
+	}
+	return num*sign;
+}
