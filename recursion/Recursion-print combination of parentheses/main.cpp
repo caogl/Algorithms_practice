738d845200f427290b55df9a9c004027d19fe957
caogl <caogl@umich.edu>
2014-09-01 23:19:12 -0400

    update

--- recursion/Recursion-print combination of parentheses/main.cpp
+++ recursion/Recursion-print combination of parentheses/main.cpp
@@ -3,41 +3,42 @@
 #include<vector>
 using namespace std;
 
-void printP(int num); // number of parenthesis
-void printP(string& out, int num, int left, int right);
+vector<string> generateParenthesis(int n);
+void generateParenthesis(int n, int left, int right, const string& tmp, vector<string>& result);
 
 int main()
 {
-	printP(3);
+	vector<string> vec=generateParenthesis(3);
+	for(int i=0; i<vec.size(); i++)
+		cout<<vec[i]<<endl;
+	cout<<endl;
 	return 0;
 }
 
-void printP(int num)
+vector<string> generateParenthesis(int n) 
 {
-	string out;
-	printP(out, num, 0, 0);
+        vector<string> result;
+        string tmp;
+        generateParenthesis(n, 0, 0, tmp, result);
+        return result;
 }
 
-void printP(string& out, int num, int left, int right)
+void generateParenthesis(int n, int left, int right, const string& tmp, vector<string>& result)
 {
-	if(left<right || left>num)
-	{
-		return;
-	}
-	if(out.size()==num*2)
-	{
-		cout<<left<<" "<<right<<" "<<out<<endl;
-		return;
-
-	}
-	out+='{';
-	left++;
-	printP(out, num, left, right);
-	left--;
-	out=out.substr(0, out.size()-1);
-	out+='}';
-	right++;
-	printP(out, num, left, right);
-	right--;
-	out=out.substr(0, out.size()-1);
+        //base case
+        if(left==n && right==n)
+        {
+            result.push_back(tmp);
+            return;
+        }
+        if(right>left || left>n)
+        {
+            return;
+        }
+        
+        string tmp1(tmp), tmp2(tmp);
+        tmp1+='(';
+        generateParenthesis(n, left+1, right, tmp1, result);
+        tmp2+=')';
+        generateParenthesis(n, left, right+1, tmp2, result);
 }
