df287a58c147d5be87f532e5e60adc65967d7f2e
caogl <caogl@umich.edu>
2014-09-03 22:31:50 -0400

    update

---
+++ stack and queue/Valid parenthesis/main.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+#include<stack>
+using namespace std;
+
+bool isValid(string s);
+
+int main()
+{
+	string s1="([)]";
+	string s2="(([{()}]))";
+	string s3="]";
+	cout<<isValid(s1)<<endl;
+	cout<<isValid(s2)<<endl;
+	cout<<isValid(s3)<<endl;
+
+	return 0;
+}
+
+bool isValid(string s)
+{
+	stack<char> stack1;
+	for(int i=0; i<s.size(); i++)
+	{
+		if(s[i]=='[' || s[i]=='{' || s[i]=='(')
+			stack1.push(s[i]);
+		else
+		{
+			if(stack1.empty())	return false;
+			if(s[i]==']' && stack1.top()!='[')	return false;
+			if(s[i]=='}' && stack1.top()!='{')	return false;
+			if(s[i]==')' && stack1.top()!='(')	return false;
+			stack1.pop();
+		}		
+	}
+	if(stack1.empty())	return true;
+	else	return false;
+}
