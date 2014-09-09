ad692fd5e493d5efa53652dc582bb5b4ba44883e
caogl <caogl@umich.edu>
2014-09-08 22:41:17 -0400

    update

---
+++ dynamic programming/Candy/main.cpp
@@ -0,0 +1,49 @@
+#include<vector>
+#include<iostream>
+using namespace std;
+
+int candy(vector<int> &ratings);
+
+int main()
+{
+	vector<int> ratings={3,2,1,2,3,4,3,2,3};
+	for(int i=0; i<ratings.size(); i++)
+		cout<<ratings[i]<<" ";
+	cout<<endl;
+	cout<<candy(ratings)<<endl;
+	return 0;
+}
+
+int candy(vector<int> &ratings)
+{
+        if(ratings.size()==0)
+            return 0;
+        vector<int> candy(ratings.size(), 1);
+	// all the non-decreasing sequence are now correct and start from 1
+        for(int i=1; i<ratings.size(); i++)
+        {
+            if(ratings[i]>ratings[i-1])
+                candy[i]=candy[i-1]+1;
+            else
+                candy[i]=1;
+        }
+        
+	// adjust the decreasing sequence
+        for(int i=ratings.size()-1; i>=1; i--)
+        {
+            if(ratings[i]<ratings[i-1] && candy[i]>=candy[i-1])
+                candy[i-1]=candy[i]+1;
+        }
+        
+	//demo of the candy distribution
+	for(int i=0; i<candy.size(); i++)
+	{
+		cout<<candy[i]<<" ";
+	}
+	cout<<endl;
+	
+        int result=0;
+        for(int i=0; i<candy.size(); i++)
+            result+=candy[i];
+        return result;
+}
