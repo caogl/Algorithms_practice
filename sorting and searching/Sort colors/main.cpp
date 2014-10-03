955b2f23a25f6f45706339b56eb0ddd34d4ed589
caogl <caogl@umich.edu>
2014-10-03 04:32:48 -0400

    update

---
+++ sorting and searching/Sort colors/main.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+
+//Note: we can use counting sort for easy solution, if not allowed use this
+void sortColors(int A[], int n);
+
+int main()
+{
+	int A[]={2,0,1,0,1,2,1,1,1,1,2,2,0,0,2,1};
+	sortColors(A, 16);
+	for(int i=0; i<16; i++)
+		cout<<A[i]<<" ";
+	return 0;
+}
+
+void sortColors(int A[], int n)
+{
+	int redStart=0;
+	int blueEnd=n-1;
+	int i=0;
+	while(i<=blueEnd)
+	{
+		if(A[i]==0)
+		{
+			A[redStart++]=0;
+			i++;
+		}
+		else if(A[i]==2)
+		{
+			swap(A[i], A[blueEnd]);
+			blueEnd--;
+		}
+		else
+			i++;
+		
+	}
+	while(redStart<=blueEnd)
+	{
+		A[redStart++]=1;
+	}
+}
