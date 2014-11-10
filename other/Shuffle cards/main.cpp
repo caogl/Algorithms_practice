d6158f419ae70ff8ff73fb6d4af1b424286ec113
caogl <caogl@umich.edu>
2014-11-10 03:18:18 -0500

    update

---
+++ other/Shuffle cards/main.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<cstdlib>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+void shuffle(vector<int>& cards)
+{
+	int n=cards.size();
+	for(int i=0; i<n; i++)
+	{
+		int j=rand()%(n-i)+i;
+		swap(cards[i], cards[j]);
+	}
+}
+
+int main()
+{
+	srand((unsigned)time(0));
+	vector<int> cards(52);
+	for(int i=0; i<52; i++)
+		cards[i]=i+1;
+	shuffle(cards);
+	cout<<"After the shuffle: "<<endl;
+	for(int i=0; i<52; i++)
+		cout<<cards[i]<<" ";
+	return 0;
+}
