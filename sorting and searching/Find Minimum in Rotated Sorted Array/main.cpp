37c266b60dc0d199d3db08e7db021b93f7c6768c
caogl <caogl@umich.edu>
2014-11-15 10:41:24 -0500

    update

---
+++ sorting and searching/Find Minimum in Rotated Sorted Array/main.cpp
@@ -0,0 +1,41 @@
+#include<vector>
+#include<iostream>
+using namespace std;
+
+int findMin(vector<int> &num);
+void findMin(vector<int> &num, int left, int right, int& result);
+
+int main()
+{
+	vector<int> num={3,4,5,6,7,8,1,2,3};
+	cout<<"The minimum is: "<<findMin(num)<<endl;
+	return 0;
+}
+
+int findMin(vector<int> &num)
+{
+	int left=0;
+	int right=num.size()-1;
+	int result=num[0];
+	findMin(num, left, right, result);
+	return result;
+}
+
+void findMin(vector<int> &num, int left, int right, int& result)
+{
+	cout<<"left is "<<left<<" right is: "<<right<<endl;
+	if(right<left)
+		return;
+	
+	int mid=(left+right)/2;
+	if(num[mid]<num[right]) // the right part must be sorted
+	{
+		result=min(result, num[mid]);
+		findMin(num, left, mid-1, result);
+	} 
+	else // the left part must be sorted
+	{
+		result=min(result, num[left]);
+		findMin(num, mid+1, right, result);
+	}
+}
