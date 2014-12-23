ada441658397019d6cfffb30ac43dac153f1cdbe
Guanglei Cao <caogl@umich.edu>
2014-12-23 02:47:10 -0500

    Update main.cpp

--- bit manipulation/grey code/main.cpp
+++ bit manipulation/grey code/main.cpp
@@ -1,4 +1,28 @@
 // reference: http://fisherlei.blogspot.com/2012/12/leetcode-gray-code.html
+/*
+[解题思路]
+看到这个题时，首先做了一个模拟，当n=3时，gray code应该是
+000
+001
+011
+010
+110
+100
+101
+111
+看了半天，也没看出来什么规律。后来上网一查GrayCode(http://en.wikipedia.org/wiki/Gray_code)才发现原来推导的gray code顺序错了。第六个应该是111。
+n=3时，正确的GrayCode应该是
+000
+001
+011
+010
+110
+111 //如果按照题意的话，只是要求有一位不同，这里也可以是100
+101
+100
+
+这样的话，规律就出来了，n=k时的Gray Code，相当于n=k-1时的Gray Code的逆序 加上 1<<k。
+*/
 
 #include<iostream>
 #include<vector>
