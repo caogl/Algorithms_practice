712b45b0ad06420e64d7cdde045948e943e8dfb3
caogl <caogl@umich.edu>
2014-12-27 17:30:11 -0500

    update

--- list/Partition list/main.cpp
+++ list/Partition list/main.cpp
@@ -1,8 +1,9 @@
 #include<iostream>
 using namespace std;
 
-/* logic:
+/*
  * find the first element in the list greater than x, then insert every element on the right that is bigger than x to the left of x, such keep stable
+ * Also use the prev->next and cur->next to keep track of the node
 */
 
 struct ListNode
@@ -30,47 +31,42 @@ int main()
 	printList(head);
 	ListNode* newHead=partition(head, 3);
 	printList(newHead);
+	//newHead=partition(head, 6);
+	//printList(newHead);
 	
 	return 0;
 }
 
-ListNode* partition(ListNode* head, int x)
+ListNode *partition(ListNode *head, int x)
 {
-	if(head==nullptr || head->next==nullptr)
-		return head;
-	ListNode* sudoHead=new ListNode(-1);
-	sudoHead->next=head;
-	ListNode* prev=sudoHead;
-	while(head!=nullptr && head->val<x)
-	{
-		head=head->next;
-		prev=prev->next;
-	}
-
-	if(head!=nullptr && head->next!=nullptr)
-	{
-		ListNode* prevCur=head;
-		ListNode* cur=prevCur->next;
-		while(cur!=nullptr)
-		{
-			if(cur->val<x)
-			{
-				prev->next=cur;
-				prev=prev->next;
-				prevCur->next=cur->next;
-				cur->next=head;
-				cur=prevCur->next;
-			}
-			else
-			{
-				prevCur=prevCur->next;
-				cur=cur->next;
-			}
-		}
-	}
-	ListNode* result=sudoHead->next;
-	delete sudoHead;
-	return result;
+        if(head==nullptr || head->next==nullptr)
+            return head;
+        ListNode* sudoHead=new ListNode(-1);
+        ListNode* prev=sudoHead;
+        sudoHead->next=head;
+        while(prev->next!=nullptr && prev->next->val<x)
+        {
+            prev=prev->next;
+        }
+        
+        ListNode* cur=prev;
+        while(prev->next!=nullptr)
+        {
+            if(prev->next->val<x)
+            {
+                ListNode* tmp=prev->next->next;
+                prev->next->next=cur->next;
+                cur->next=prev->next;
+		cur=cur->next; // do not change the relative order of the numbers smallers than x !!!
+                prev->next=tmp;
+            }
+            else
+                prev=prev->next;
+        }
+        
+        head=sudoHead->next;
+        delete sudoHead;
+        return head;
 }
 
 void printList(ListNode* head)
