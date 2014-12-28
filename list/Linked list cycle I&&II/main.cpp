15deb9e1951993413db74d35274803efd8f8e982
Guanglei Cao <caogl@umich.edu>
2014-12-27 23:37:23 -0500

    Update main.cpp

--- list/Linked list cycle I&&II/main.cpp
+++ list/Linked list cycle I&&II/main.cpp
@@ -1,3 +1,14 @@
+// reference: http://blog.csdn.net/kenden23/article/details/13871699
+/*
+1. 假设圈的周长L, a is the distance before entering the circle, b is the distance between the cycle node
+   and the meeting node
+2. 那么相遇的时候slow走：a + b,而fast走：a + b + n*L，（n代表fast走了多少圈）
+3. fast走路的路程是slow的两倍，那么2(a+b) = a + b + n*L，得到a = n*L - b
+4 从相遇点的时候开始，放一个指针从开始点走起，另一个指针继续走，而且这时走的速度都是一样的，
+  那么当一个指针从开始点X走到循环圈点Y的时候，走了a路程，而另一个指针走的路程是n*L-b，
+  那么两者的路程是一样的，相遇点必然是Y。
+*/
+
 #include<iostream>
 using namespace std;
 
