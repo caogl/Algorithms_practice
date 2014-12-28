d48a4a594a61f1d94989b80f7afbf179b9bb63bb
caogl <caogl@umich.edu>
2014-12-27 20:18:53 -0500

    update

--- list/Reorder list/main.cpp
+++ list/Reorder list/main.cpp
@@ -1,10 +1,7 @@
+// reference: http://yucoding.blogspot.com/2013/12/leetcode-question-reorder-list.html
+
 #include<iostream>
 using namespace std;
-
-/* logic:
- * http://yucoding.blogspot.com/2013/12/leetcode-question-reorder-list.html
- * !! notice the corner case when k>=length of list
-*/
 
 struct ListNode
 {
@@ -24,11 +21,11 @@ int main()
 {
 	ListNode* head=new ListNode(1);
 	head->next=new ListNode(2);
-	//head->next->next=new ListNode(3);
-	//head->next->next->next=new ListNode(4);
-	//head->next->next->next->next=new ListNode(5);
-	//head->next->next->next->next->next=new ListNode(6);
-	//head->next->next->next->next->next->next=new ListNode(7);
+	head->next->next=new ListNode(3);
+	head->next->next->next=new ListNode(4);
+	head->next->next->next->next=new ListNode(5);
+	head->next->next->next->next->next=new ListNode(6);
+	head->next->next->next->next->next->next=new ListNode(7);
 	printList(head);
 
 	reorderList(head);
@@ -36,61 +33,55 @@ int main()
 	return 0;
 }
 
-void reorderList(ListNode *head)
+void reorderList(ListNode *head) 
 {
-	if(head==nullptr || head->next==nullptr)
-		return;
+        if(head==nullptr || head->next==nullptr || head->next->next==nullptr)
+            return;
 
-	// step 1: find the middle node and split
-	ListNode *head0=head;
-	ListNode *head1=head;
-	ListNode *first=head;
-	ListNode *second=head;
-	while(true)
-	{
-		second=second->next;
-		if(second==nullptr)
-			break;
-		second=second->next;
-		if(second==nullptr)
-			break;
-		first=first->next;
-	}
-	head1=first->next; // the head of the second list
-	first->next=nullptr; // split
-	
-	// step 2: reverse the second list
-	ListNode* cur=head1;
-	ListNode* post=head1->next;
-	ListNode* temp;
-	while(post!=nullptr)
-	{
-		temp=post->next;
-		post->next=cur;
-		cur=post; 
-		post=temp;
-	}
-	head1->next=nullptr;
-	head1=cur;
-	
-	// step 3: merge two list
-	cur=head;
-	head0=head0->next;
-	while(head0!=nullptr || head1!=nullptr)
-	{
-		if(head1!=nullptr)
-		{
-			cur->next=head1;
-			cur=cur->next;
-			head1=head1->next;
-		}
-		if(head0!=nullptr)
-		{
-			cur->next=head0;
-			cur=cur->next;
-			head0=head0->next;
-		}
-	}
+        /* step(1): partition */
+        ListNode* head1=head;
+        ListNode* head2=head;
+        head2=head2->next;
+        while(head2!=nullptr && head2->next!=nullptr)
+        {
+            head2=head2->next->next;
+            head1=head1->next;
+        }
+        head2=head1->next;
+        head1->next=nullptr;
+
+        /* step(2): reorder second list */
+        ListNode* prev=head2; // cannot be nullptr since the length of the list is at least 2
+        ListNode* cur=prev->next;
+        prev->next=nullptr;
+        ListNode* post;
+        while(cur!=nullptr)
+        {
+            post=cur->next;
+            cur->next=prev;
+            prev=cur;
+            cur=post;
+        }
+        head2=prev;
+        head1=head;
+
+        /* step(3): merge two list */
+        head1=head1->next; // This step necessary! or when head=head2, head1 still equals head, mess......
+        while(head1!=nullptr || head2!=nullptr)
+        {
+            if(head2!=nullptr)
+            {
+                head->next=head2;
+                head2=head2->next;
+                head=head->next;
+            }
+            if(head1!=nullptr)
+            {
+                head->next=head1;
+                head1=head1->next;
+                head=head->next;
+            }
+        }
 }
 
 void printList(ListNode* head)
@@ -102,4 +93,3 @@ void printList(ListNode* head)
 	}
 	cout<<endl;
 }
-
