b248f74e9e0fa8a858208e9127edd8d9a0511e1f
caogl <caogl@umich.edu>
2014-12-30 03:55:47 -0500

    update

--- recursion/N queue--Backtracking /main.cpp
+++ recursion/N queue--Backtracking /main.cpp
@@ -4,8 +4,8 @@
 using namespace std;
 
 vector<vector<string> > solveNQueens(int n);
-void solveNQueens(int n, int cur, vector<int>& tmp, vector<vector<string> >& result);
-vector<string> convert(vector<int>& vec);
+void solveNQueens(vector<vector<string> >& result, vector<int>& tmp, int current, int n);
+vector<string> convert(vector<int>& tmp);
 
 int main()
 {
@@ -20,83 +20,49 @@ int main()
 	return 0;
 }
 
-vector<vector<string> > solveNQueens(int n)
+vector<vector<string> > solveNQueens(int n) 
 {
-        int cur=0;
         vector<vector<string> > result;
         vector<int> tmp;
-        solveNQueens(n, cur, tmp, result);
+        int current;
+        solveNQueens(result, tmp, 0, n);
         return result;
 }
 
-
-void solveNQueens(int n, int cur, vector<int>& tmp, vector<vector<string> >& result)
+void solveNQueens(vector<vector<string> >& result, vector<int>& tmp, int current, int n)
 {
-        if(cur==n)
+        if(current==n)
         {
-            vector<string> Queen=convert(tmp);
-            result.push_back(Queen);
+            vector<string> vec=convert(tmp);
+            result.push_back(vec);
             return;
         }
         
         for(int i=0; i<n; i++)
         {
-            vector<int> tmp1(tmp);
-	    tmp1.push_back(i);
-            bool prune=false;
-            for(int j=0; j<cur; j++)
+            tmp.push_back(i);
+            bool promise=true;
+            for(int j=0; j<current; j++) // easy to cause bug: tend to use i instead of current 
             {
-                if(tmp1[j]==tmp1[cur] || abs(cur-j)==abs(tmp1[cur]-tmp1[j]))
+                if(tmp[current]==tmp[j] || current-j==abs(tmp[current]-tmp[j]))
                 {
-                    prune=true;
+                    promise=false;
                     break;
                 }
             }
-            if(prune==false)
-            {
-                solveNQueens(n, cur+1, tmp1, result);
-            }
+            if(promise)
+                solveNQueens(result, tmp, current+1, n);
+            tmp.pop_back();
         }
 }
 
-/* buggy solution:: easy to make this kind of mistake
-void solveNQueens(int n, int cur, vector<int>& tmp, vector<vector<string> >& result)
-{
-        if(cur==n)
-        {
-            vector<string> Queen=convert(tmp);
-            result.push_back(Queen);
-            return;
-        }
-        
-        for(int i=0; i<n; i++)
-        {
-            tmp.push_back(i); // bug(1): tmp keeps increasing the size, wrong
-            bool prune=false;
-            for(int j=0; j<cur; j++)
-            {
-                if(tmp[j]==tmp[i] || abs(i-j)==abs(tmp[i]-tmp[j])) //bug(2)
-                {
-                    prune=true;
-                    break;
-                }
-            }
-            if(prune==false)
-            {
-                vector<int> tmp1(tmp);
-                solveNQueens(n, cur+1, tmp1, result);
-            }
-        }
-}
-*/
-
-vector<string> convert(vector<int>& vec)
+vector<string> convert(vector<int>& tmp)
 {
         vector<string> result;
-        for(int i=0; i<vec.size(); i++)
+        for(int i=0; i<tmp.size(); i++)
         {
-            string str(vec.size(), '.');
-            str[vec[i]]='Q';
+            string str(tmp.size(), '.');
+            str[tmp[i]]='Q';
             result.push_back(str);
         }
         return result;
