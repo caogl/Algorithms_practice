5db30ac5cbe5104af44d3a56b46e2cda75f0f26d
Guanglei Cao <caogl@umich.edu>
2015-01-04 02:18:46 -0500

    Update main.cpp

--- string/Minimum window substring/main.cpp
+++ string/Minimum window substring/main.cpp
@@ -1,6 +1,6 @@
 // reference: http://fisherlei.blogspot.com/2012/12/leetcode-minimum-window-substring.html
-// 双指针，动态维护一个sliding window。尾指针不断往后扫，当扫到有一个窗口包含了所有T的字符后
-// ，然后再收缩头指针，直到不能再收缩为止。最后记录所有可能的情况中窗口最小的
+// 双指针，动态维护一个sliding window。尾指针不断往后扫，当扫到有一个窗口包含了所有T的字符后,
+// 然后再收缩头指针，直到不能再收缩为止。最后记录所有可能的情况中窗口最小的
 
 
 #include<unordered_map>
