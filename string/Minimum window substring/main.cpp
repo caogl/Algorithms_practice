6f3e05586a8d22acf76d67a5c80870e9949781c5
caogl <caogl@umich.edu>
2015-01-04 02:14:21 -0500

    update

--- string/Minimum window substring/main.cpp
+++ string/Minimum window substring/main.cpp
@@ -1,9 +1,8 @@
-// This code cannot pass the largest test case in leetcode, time exceeded
-// The reason is the use of hash table, to pass that case, must use array as a hash table
-// But the idea and result is right 
-// For correct code, refer to http://fisherlei.blogspot.com/2012/12/leetcode-minimum-window-substring.html
+// reference: http://fisherlei.blogspot.com/2012/12/leetcode-minimum-window-substring.html
+
 #include<unordered_map>
 #include<string>
+#include<climits>
 #include<iostream>
 using namespace std;
 
@@ -11,99 +10,60 @@ string minWindow(string S, string T);
 
 int main()
 {	
-	string s1="ADOBECODEBANC";
-	string t1="ABC";
-	//string s1="aa";
-	//string t1="aa";
+	string s1="bdab";
+	string t1="ab";
 	cout<<"The minimum window substring is: "<<minWindow(s1, t1)<<endl;
 	return 0;
 }
 
-string minWindow(string S, string T) 
+string minWindow(string S, string T)
 {
-	string result="";
-	if(S.size()==0 || T.size()==0 || S.size()<T.size())
-		return result;
-
-        unordered_map<char, int> times, times1, times2;
+        if(T.size()==0 || T.size()>S.size())
+            return "";
+        
+        int n=T.size();
+        unordered_map<char, int> expected;
         for(int i=0; i<T.size(); i++)
+            expected[T[i]]++;
+        
+        unordered_map<char, int> matched;
+        for(auto itr=expected.begin(); itr!=expected.end(); itr++)
+            matched[itr->first]=0;
+        
+        int minLen=INT_MAX;
+        string result;
+        int slow=0;
+        int appeared=0;
+
+        for(int fast=0; fast<S.size(); fast++)
         {
-		if(times.find(T[i])==times.end())
-			times[T[i]]=1;
-		else
-		{
-			times[T[i]]++;
-		}
-        }
-	times1=times;
-	for(unordered_map<char, int>::iterator itr=times.begin(); itr!=times.end(); itr++)
-		times2[itr->first]=0;
-        string tmp;
-        int start=0;
+            if(expected.find(S[fast])!=expected.end())
+            {				
+                if(matched[S[fast]]<expected[S[fast]])
+                {
+                    appeared++;
+                    matched[S[fast]]++;
+                    if(appeared==n) // sliding window matched T
+                    {
+                        while(expected.find(S[slow])==expected.end() || matched[S[slow]]>expected[S[slow]])
+                        {
+                            matched[S[slow]]--;
+                            slow++;
+                        }
+                        if(minLen>fast-slow+1)
+                        {
+                            minLen=fast-slow+1;
+                            result=S.substr(slow, minLen);
+                        }
 
-        while(start<S.size())
-	{
-		// move start position so that S[start] is a part of T
-		int i=start;
-		while(times.find(S[i])==times.end())  
-		{
-			i++;
-			if(i==S.size())
-				return result;
-		}
-		start=i;
-
-		// find a substring meet the requirement
-		while(true)
-            	{
-                	if(times.find(S[i])!=times.end())
-                	{
-                    		times2[S[i]]++;
-                    		if(times1.find(S[i])!=times1.end())
-                    		{	
-                       			if(times1[S[i]]==1)
-					{
-						times1.erase(S[i]);
-						if(times1.empty())
-							break;
-					}
-					else
-					{
-						times1[S[i]]--;
-					}
-                    		}
-                	}
-                	i++;
-                	if(i>=S.size())
-                    		return result;
-            	}
-
-		// move the start position forward to squeeze the width of the substring, still valid
-		while(true)
-		{
-			if(times2.find(S[start])==times2.end())
-				start++;
-			else if(times2[S[start]]>times[S[start]])
-			{
-				times2[S[start]]--;
-				start++;
-			}
-			else
-				break;
-		}
-
-            	tmp=S.substr(start, i-start+1);
-		if(result=="")
-			result=tmp;
-		else if(tmp.size()<result.size())
-		{
-                	result=tmp;
-		}
-
-		times1=times;
-		for(unordered_map<char, int>::iterator itr=times.begin(); itr!=times.end(); itr++)
-			times2[itr->first]=0;
-            	start=start+1;
+			matched[S[slow]]--;
+                        slow++;
+                        appeared--;
+                    }
+                }
+                else
+                    matched[S[fast]]++;
+            }
         }
         return result;
 }
