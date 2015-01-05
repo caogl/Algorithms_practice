c4c78d617723239afb6d81d46c2cd1e52fc02b82
caogl <caogl@umich.edu>
2015-01-04 21:03:56 -0500

    update

--- other/Maximal rectangle/main.cpp
+++ other/Maximal rectangle/main.cpp
@@ -1,4 +1,27 @@
+// reference: http://www.cnblogs.com/TenosDoIt/p/3454877.html
+/* demo: 
+ *
+rectangle:
+1 1 1 1 0 0 
+1 1 1 0 1 1 
+1 0 1 0 1 1 
+0 1 1 1 1 1 
+1 1 1 1 1 0 
+0 1 0 1 1 0
+
+dp matrix:
+4 3 2 1 0 0 
+3 2 1 0 2 1 
+1 0 1 0 2 1 
+0 5 4 3 2 1 
+5 4 3 2 1 0 
+0 1 0 2 1 0
+
+maxV; 8
+*/
+
 #include<vector>
+#include<cstring> //for memset
 #include<iostream>
 #include<algorithm>
 using namespace std;
@@ -8,37 +31,18 @@ int largestRectangleArea(vector<int> &height);
 
 int main()
 {
-	/*
- *
-	char matrix[][8]={{'1','1','1','1','1','1','0','1'},{'1','0','1','0','1','1','1','1'}, {'1','1','0','1','1','1','1','0'}, {'1','1','1','0','1','1','1','0'}};
+	char matrix[][6]={{'1','1','1','1','0','0'},{'1','1','1','0','1','1'},{'1','0','1','0','1','1'},{'0','1','1','1','1','1'},{'1','1','1','1','1','0'},{'0','1','0','1','1','0'}};
 
-	vector<char> temp(8);
-	vector<vector<char> > vec(4, temp);
-	
-	for(int i=0; i<4; i++)
-		for(int j=0; j<8; j++)
-			vec[i][j]=matrix[i][j];
-
-	for(int i=0; i<4; i++)
-	{
-		for(int j=0; j<8; j++)
-			cout<<matrix[i][j]<<" ";
-		cout<<endl;
-	}
-	*/
-
-	char matrix[][5]={{'0','1','1','0','1'},{'1','1','0','1','0'},{'0','1','1','1','0'},{'1','1','1','1','0'},{'1','1','1','1','1'},{'0','0','0','0','0'}};
-
-	vector<char> temp(5);
+	vector<char> temp(6);
 	vector<vector<char> > vec(6, temp);
 
 	for(int i=0; i<6; i++)
-		for(int j=0; j<5; j++)
+		for(int j=0; j<6; j++)
 			vec[i][j]=matrix[i][j];
 
 	for(int i=0; i<6; i++)
 	{
-		for(int j=0; j<5; j++)
+		for(int j=0; j<6; j++)
 			cout<<matrix[i][j]<<" ";
 		cout<<endl;
 	}
@@ -46,6 +50,39 @@ int main()
 	cout<<"Max rectangle size is: "<<maximalRectangle(vec)<<endl;
 }
 
+int maximalRectangle(vector<vector<char> > &matrix)
+{
+	if(matrix.size()==0 || matrix[0].size()==0)
+		return 0
+	int rowNum=matrix.size();
+	int colNum=matrix[0].size();
+	int dp[rowNum][colNum];
+	memset(dp, 0, sizeof(dp));
+	for(int i=0; i<rowNum; i++)
+		dp[i][colNum-1]=(matrix[i][colNum-1]=='1');
+	for(int i=0; i<rowNum; i++)
+		for(int j=colNum-2; j>=0; j--)
+			if(matrix[i][j]=='1')
+				dp[i][j]=1+dp[i][j+1];
+	int maxV=0;	
+
+	for(int i=0; i<rowNum; i++)
+	{
+		for(int j=0; j<colNum; j++)
+		{	
+			int width=dp[i][j];
+			for(int k=i; k<rowNum && width>0; k++)
+			{
+				width=min(width, dp[k][j]);
+				if(width*(rowNum-i)<=maxV)	break;  // branch and bound
+				maxV=max(maxV, width*(k-i+1));				
+			}
+		}
+	}
+	return maxV;
+}
+
+/* solution based on "Maximal rectangle in histogram"
 int maximalRectangle(vector<vector<char> > &matrix)
 {
 	if(matrix.size()==0 || matrix[0].size()==0)
@@ -113,3 +150,4 @@ int largestRectangleArea(vector<int> &height)
 	}
 	return maxSize;
 }
+*/
