1acd33fcde035cd61e6addb5dd7cca9793b0326e
Guanglei Cao <caogl@umich.edu>
2015-01-05 00:49:26 -0500

    Update main.cpp

--- other/Two sum III - Data structure design/main.cpp
+++ other/Two sum III - Data structure design/main.cpp
@@ -1,46 +1,29 @@
-#include<iostream>
-#include<map>
-#include<vector>
-using namespace std;
-
-vector<int> twoSum(vector<int> &numbers, int target);
-
-int main()
+class TwoSum 
 {
-	vector<int> vec;
-	vec.push_back(2);
-	vec.push_back(3);
-	vec.push_back(4);
-	vec.push_back(5);
-	vec.push_back(6);
-	vec.push_back(7);
-	vec.push_back(8);
-	vec.push_back(9);
-	vec.push_back(11);
-	vector<int> result=twoSum(vec, 6);
-	for(int i=0; i<vec.size(); i++)
-		cout<<vec[i]<<" ";
-	cout<<endl;
-	cout<<result[0]<<" "<<result[1]<<endl;
-	return 0;
-}
+public:
+	void add(int number) 
+	{
+	    hashMap[number]++;
+	}
 
-/* because sorted: time--O(n), space--O(1) */
-vector<int> twoSum(vector<int> &numbers, int target) 
-{
-        vector<int> result;
-        if(numbers.size()<2)
-            return result;
-        int i=0;
-        int j=numbers.size()-1;
-        int sum=numbers[i]+numbers[j];
-        while(sum!=target)
-        {
-            if(sum>target)	j--;
-            else    i++;
-            sum=numbers[i]+numbers[j];
-        }
-        result.push_back(i+1);
-        result.push_back(j+1);
-        return result;
-}
+	bool find(int value) 
+	{
+	    for(auto itr=hashMap.begin(); itr!=hashMap.end(); itr++)
+	    {
+	        int tmp=value-itr->first;
+	        if(hashMap.find(tmp)!=hashMap.end()) // deal with self duplicate
+	        {
+	            if(tmp==itr->first)
+	            {
+	                if(hashMap[tmp]>1)
+	                    return true;
+	                continue;
+	            }
+	            return true;
+	        }
+	    }
+	    return false;
+	}
+private:	
+	unordered_map<int, int> hashMap;
+};
