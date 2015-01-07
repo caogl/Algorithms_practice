f0039e7d124da7fe398281d29e32444cf0e9b341
Guanglei Cao <caogl@umich.edu>
2015-01-06 23:19:27 -0500

    Update main.cpp

--- string/One edit distance/main.cpp
+++ string/One edit distance/main.cpp
@@ -1,5 +1,29 @@
 // reference: leetcode clean code handbook
 // The definition of edit is the same in the problem "Edit Distance", ONLY return distance 1, and 0 distance is not included!
+/*
+O(n) runtime, O(1) space – Simple one-pass:
+
+For the case where m is equal to n, it becomes finding if there is exactly one modified 
+character. Now let’s assume m ≤ n. (If m > n we could just swap them).
+Assume X represents the one-edit character. There are three one-edit distance operations 
+that could be applied to S.
+(i). Modify operation – Modify a character to X in S.
+S = “abcde”
+T = “abXde”
+(ii). Insert operation – X was inserted before a character in S.
+S = “abcde”
+T = “abcXde”
+(iii). Append operation – X was appended at the end of S.
+S = “abcde”
+T = “abcdeX”
+
+We make a first pass over S and T concurrently and stop at the first non-matching character between S and T.
+(1). If S matches all characters in T, then check if there is an extra character at the end  of T. (Append)
+(2). If | n – m | == 1, that means we must skip this non-matching character only in T
+and make sure the remaining characters between S and T are exactly matching. (Insert)
+(3). If | n – m | == 0, then we skip both non-matching characters in S and T and make 
+sure the remaining characters between S and T are exactly matching. (Modify)
+*/
 
 #include<string>
 #include<iostream>
