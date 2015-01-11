06d5e3c4b13752e5093f1bcd4a82116d25ac7e60
caogl <caogl@umich.edu>
2015-01-11 17:54:58 -0500

    update

---
+++ recursion/Longest consecutive sequence in 2D matrix/main.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int help(vector<vector<int> > &matrix, vector<vector<int> > &info, int i, int j)
+{
+// This recursive solution is in courtesy of Lai Ting. Once return, the max value of the current node is determined
+        if (!info[i][j])
+	{
+                info[i][j] = 1;
+                if (i && matrix[i-1][j] == matrix[i][j]+1) info[i][j] = max(info[i][j], help(matrix, info, i-1, j) + 1);
+                if (i != matrix.size()-1 && matrix[i+1][j] == matrix[i][j]+1) info[i][j] = max(info[i][j], help(matrix, info, i+1, j) + 1);
+                if (j && matrix[i][j-1] == matrix[i][j]+1) info[i][j] = max(info[i][j], help(matrix, info, i, j-1) + 1);
+                if (j != matrix[0].size()-1 && matrix[i][j+1] == matrix[i][j]+1) info[i][j] = max(info[i][j], help(matrix, info, i, j+1) + 1);
+        }
+        return info[i][j];
+}
+
+int longestPath(vector<vector<int> > &matrix)
+{
+        if (matrix.empty() && matrix[0].empty()) return 0;
+
+        int row = matrix.size();
+        int col = matrix[0].size();
+
+        vector<vector<int> > info(row, vector<int>(col, 0)); // store the info of the longest length of current node
+        int res = 1;
+
+        for (int i=0; i<row; i++)
+                for (int j=0; j<col; j++) // O(mn), every node is processed once
+                        res = max(res, help(matrix, info, i, j));
+        return res;
+}
+
+int main(){
+
+        vector<vector<int> > matrix;
+        matrix.push_back(vector<int>({2,3,4,5}));
+        matrix.push_back(vector<int>({4,5,10,11}));
+        matrix.push_back(vector<int>({20,6,9,12}));   // modify the test case
+	matrix.push_back(vector<int>({6,7,8,40}));
+        cout << longestPath(matrix) << endl;
+
+        return 0;
+}
