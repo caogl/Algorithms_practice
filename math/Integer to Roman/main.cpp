0435ddec96254837419cb6c5b63d6f9e49e38d89
caogl <caogl@umich.edu>
2015-01-14 12:16:51 -0500

    update

--- math/Integer to Roman/main.cpp
+++ math/Integer to Roman/main.cpp
@@ -14,79 +14,33 @@ entity to avoid dealing with these special cases to greatly simplify the code.
 
 #include<iostream>
 #include<string>
+#include<vector>
 using namespace std;
 
 string intToRoman(int num);
 
 int main()
 {
-	cout<<intToRoman(3923)<<endl;
+	cout<<intToRoman(49)<<endl; // XLIX
 	return 0;
 }
 
 string intToRoman(int num)
 {
-        string result;
-        int digit=(int)(num/1000);
-        for(int i=0; i<digit; i++)
-            result+='M';
-       
-        num=num%1000;
-        digit=(int)(num/100);
-        if(digit!=0)
-        {
-            if(digit<4)
-                for(int i=0; i<digit; i++)
-                    result+='C';
-            else if(digit==4)
-                result+="CD";
-            else if(digit<=8)
-            {
-                result+='D';
-                for(int i=5; i<digit; i++)
-                    result+='C';
-            }
-            else // digit==9
-                result+="CM";
-        }
-        
-        num=num%100;
-        digit=(int)(num/10);
-        if(digit!=0)
-        {
-            if(digit<4)
-                for(int i=0; i<digit; i++)
-                    result+='X';
-            else if(digit==4)
-                result+="XL";
-            else if(digit<=8)
-            {
-                result+='L';
-                for(int i=5; i<digit; i++)
-                    result+='X';
-            }
-            else // digit==9
-                result+="XC";
-        }
-        
-        num=num%10;
-        digit=num;
-        if(digit!=0)
-        {
-            if(digit<4)
-                for(int i=0; i<digit; i++)
-                    result+='I';
-            else if(digit==4)
-                result+="IV";
-            else if(digit<=8)
-            {
-                result+='V';
-                for(int i=5; i<digit; i++)
-                    result+='I';
-            }
-            else // digit==9
-                result+="IX";
-        }
-        
+	vector<int> values={1000,900,500,400,100,90,50,40,10,9,5,4,1};
+	vector<string> symbols={"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
+	string result;
+	int i=0;
+	while(num>0)
+	{
+		int k=num/values[i];
+		for(int j=0; j<k; j++)
+		{
+			result+=symbols[i];
+			num-=values[i];
+		}
+		i++;
+	}
+	return result;
         return result;
 }
