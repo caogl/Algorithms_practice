3e995eb1715183fae61d694855c90b0bd9094576
Guanglei Cao <caogl@umich.edu>
2015-01-14 12:00:36 -0500

    Update main.cpp

--- math/Integer to Roman/main.cpp
+++ math/Integer to Roman/main.cpp
@@ -1,3 +1,17 @@
+/* reference: leetcode cleancode official handbook
+The subtractive notation:
+Four characters are avoided being repeated in succession (such as IIII). Instead, the 
+symbol I could appear before V and X to signify 4 (IV) and 9 (IX) respectively. Using the 
+same pattern, we observe that X could appear before L and C to signify 40 (XL) and 90 
+(XC) respectively. The same pattern could be applied to C that is placed before D and M.
+
+Using the additive notation, we convert to roman numerals by breaking it so each chunk 
+can be represented by the symbol entity. For example, 11 = 10 + 1 = “X” + “I”. Similarly, 
+6 = 5 + 1 = “V” + “I”. Let’s take a look of an example which uses the subtractive
+notation: 49 = 40 + 9 = “XL” + “IX”. Note that we treat “XL” and “IX” as one single 
+entity to avoid dealing with these special cases to greatly simplify the code.
+*/
+
 #include<iostream>
 #include<string>
 using namespace std;
