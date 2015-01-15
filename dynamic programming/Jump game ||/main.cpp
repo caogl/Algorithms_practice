b4c6ea5dce4300a358f783eca04594fbcd42f8ed
Guanglei Cao <caogl@umich.edu>
2015-01-14 21:06:40 -0500

    Update main.cpp

--- dynamic programming/Jump game ||/main.cpp
+++ dynamic programming/Jump game ||/main.cpp
@@ -24,14 +24,14 @@ int jump(int A[], int n)
             
         int minStep[n];
         minStep[0]=0;
-        int reached=0;
+        int reached=0; // initialize reached to be 0 rather than A[0], different from Jump Game I !!
             
         for(int i=0; i<n; i++)
         {
             if(A[i]+i>=n-1)
                 return minStep[i]+1;
             /* only need to update smallest distance from the farest can be reached in previous positions*/
-            for(int j=reached+1; j<=A[i]+i; j++) 
+            for(int j=reached+1; j<=A[i]+i; j++) // notice "<="!
                 minStep[j]=minStep[i]+1;
             reached=max(reached, A[i]+i);
         }
