3d2664bd9a0dcc35ff0e9b121be90272a209c874
caogl <caogl@umich.edu>
2015-01-22 16:00:23 -0500

    update

--- dynamic programming/Longest common substring/main.cpp
+++ dynamic programming/Longest common substring/main.cpp
@@ -1,18 +1,52 @@
 // reference: http://www.geeksforgeeks.org/longest-common-substring/
+// Google Interview question: optimize the space usage 
+//			      --> optimize to linear at best
 
+#include<cstring>
 #include<iostream>
 #include<string>
 #include<vector>
 using namespace std;
 
-int LongestCommonSubstring(string &s1, string &s2);
+int LongestCommonSubstring(string &s1, string &s2); // space: O(m*n)
+int LCSGoogle(string s1, string s2); // space: O(n)
 
 int main()
 {
 	string s1="Site:GeeksforGeekshahahahaa.org";
 	string s2="NewSite:GeeksQuizhahahahab.com";
 	cout<<LongestCommonSubstring(s1, s2)<<endl;
+	cout<<LCSGoogle(s1, s2)<<endl;
 	return 0;
+}
+
+int LCSGoogle(string s1, string s2)
+{
+	int dp[s2.size()+1]; 
+	int i=0;
+	int maxLen=-1;
+	memset(dp, 0, sizeof(dp));
+	for(i=1; i<=s1.size(); i++)
+	{
+		// Important to follow this order, or  it will produce a "gradually replace" problem!!!
+		/*	0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 
+			0 0 0 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 
+			0 0 0 0 1 0 0 0 0 0 0 0 0 0 0 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 
+			0 0 0 0 0 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 
+			0 1 0 0 0 0 1 0 0 1 2 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 
+ 		*/
+		for(int j=s2.size(); j>=1; j--)
+		{	
+			if(s1[i-1]==s2[j-1])
+			{
+				maxLen=max(maxLen, dp[j-1]+1);
+				dp[j]=dp[j-1]+1;
+			}
+			else
+				dp[j]=0;
+		}
+	}
+	return maxLen;
 }
 
 int LongestCommonSubstring(string &s1, string &s2)
