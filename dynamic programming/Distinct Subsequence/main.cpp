105dad85b5bb67dbf6544d97df1c15a22d0cc4cd
caogl <caogl@umich.edu>
2015-01-22 23:13:50 -0500

    update

--- dynamic programming/Distinct Subsequence/main.cpp
+++ dynamic programming/Distinct Subsequence/main.cpp
@@ -6,34 +6,39 @@
 #include<cstring>
 using namespace std;
 
+/* solution (1): Resurison: time complexcity: O(2^n), space: O(n);
+int numDistinct(string S, string T);
+void numDistinct(string S, int Spos, string T, int Tpos, int& num); */
+
+/* solution (2): Dynamic Programming: time complexicty: O(n^2), space: O(n^2) */
 int numDistinct(string S, string T);
 
-/* the recursive solution, more natural for this case, but runtime error for big test cases---call stacks!
-int numDistinct(string S, string T);
-void numDistinct(string S, int Spos, string T, int Tpos, int& num);
-*/
 
 int main()
 {
-	string S="chuuchunleiunllei";
-	string T="chunlei";
-	cout<<numDistinct(S, T)<<endl;	
+	string S="rabbbit";
+	string T="rabbit";
+	cout<<numDistinct(S, T)<<endl;
+
+	S="aabdbaabeeadcbbdedacbbeecbabebaeeecaeabaedadcbdbcdaabebdadbbaeabdadeaabbabbecebbebcaddaacccebeaeedababedeacdeaaaeeaecbe";
+	T="bddabdcae";
+	cout<<numDistinct(S, T)<<endl;	// recursive solution time limit exceeded
 	return 0;
 }
 
+/* solution (1) */
 int numDistinct(string S, string T)
 {
 	int lenS=S.size();
 	int lenT=T.size();
 	/* notice corner cases!!! */
-	if(lenT==0)
-		return 1;
-	if(lenS==0)
-		return 0;
+	if(lenT==0)	return 1;
+	if(lenS==0)	return 0;
 	int dp[lenS+1][lenT+1]; // size to be [+1][+1]
-	memset(dp, 0, sizeof(dp));
 	
-	/* starting values !!! */
+	/* starting values, the orders of two for loops cannot be exchanged to ensure that dp[0][0]=1!!! */
+	for(int i=0; i<=lenT; i++)
+		dp[0][i]=0;
 	for(int i=0; i<=lenS; i++)
 		dp[i][0]=1;
 
@@ -51,28 +56,30 @@ int numDistinct(string S, string T)
 	return dp[lenS][lenT];
 }
 
-/*
+/* solution(2)
 int numDistinct(string S, string T)
 {
 	int num=0;
-	numDistinct(S, S.size()-1, T, T.size()-1, num);
+	numDistinct(S, 0, T, 0, num);
 	return num;
 }
 
 void numDistinct(string S, int Spos, string T, int Tpos, int& num)
 {
-	if(Tpos<0)
-	{
-		num++;
-		return;
-	}
-	if(Spos<0)
-		return;
-	while(S[Spos]!=T[Tpos])
-		Spos--;
-	if(Spos<0)
-		return;
-	numDistinct(S, Spos-1, T, Tpos-1, num);
-	numDistinct(S, Spos-1, T, Tpos, num);
+        if(sPos==S.size())  return;
+        if(tPos==T.size())
+        {
+            num++;
+            return;
+        }
+
+        if(S[sPos]==T[tPos])
+        {
+            numDist(S, sPos+1, T, tPos+1, num);
+            numDist(S, sPos+1, T, tPos, num);
+        }
+        else
+            numDist(S, sPos+1, T, tPos, num);
+        }
 }
 */
