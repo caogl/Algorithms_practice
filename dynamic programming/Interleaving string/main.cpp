8e6f3f7597deb7d5bef5a5f0283694eb13b11c30
caogl <caogl@umich.edu>
2015-01-23 00:16:54 -0500

    update

--- dynamic programming/Interleaving string/main.cpp
+++ dynamic programming/Interleaving string/main.cpp
@@ -5,10 +5,11 @@
 #include<iostream>
 using namespace std;
 
-bool isInterleave(string s1, string s2, string s3);
+/* Solution (1): Recusion, time complexity: O(2^n), n=min(s1.size(), s2.size()), space: O(n), n=max(s1.size(), s2.size())
+void isInterleave(string s1, string s2, string s3, bool& match); */
 
-// recursive solution, time limit exceed
-//void isInterleave(string& s1, string& s2, string& s3, bool& match);
+/* Solution (2): Dynamic Programming, time complexity: O(m*n), space: O(m*n)*/
+bool isInterleave(string s1, string s2, string s3);
 
 int main()
 {
@@ -19,6 +20,7 @@ int main()
 	return 0;
 }
 
+/* Solution (2)*/
 bool isInterleave(string s1, string s2, string s3)
 {
 	int n1=s1.size();
@@ -26,9 +28,10 @@ bool isInterleave(string s1, string s2, string s3)
 	int n3=s3.size();
 	if(n1+n2!=n3)
 		return false;
-	vector<vector<bool> > dp(n1+1, vector<bool> (n2+1, false));
-	
-	dp[0][0]=true;
+
+	//dp[i][j] means whether s1[0...i-1] and s2[0...j-1] can be interleaved to s3[0...i+j-1]
+	vector<vector<bool> > dp(n1+1, vector<bool> (n2+1, false));	
+	dp[0][0]=true; // important initialization
 
 	/* Be very careful about the index of the strings --- i-1 !!!*/
 	for(int i=1; i<=n1; i++)
@@ -49,41 +52,27 @@ bool isInterleave(string s1, string s2, string s3)
 	return dp[n1][n2];
 }
 
-/* recursive solution
+/* solution (1)
 bool isInterleave(string s1, string s2, string s3)
 {
         if(s3.size()!=s1.size()+s2.size())
             return false;
         bool match=false;
-        isInterleave(s1, s2, s3, match);
+        isInterleaving(s1, s2, s3, match);
         return match;
 }
 
-void isInterleave(string& s1, string& s2, string& s3, bool& match)
+void isInterleaving(string s1, string s2, string s3, bool& match)
 {
+        if(match)   return;
         if(s1.size()==0 && s2.size()==0)
         {
             match=true;
             return;
         }
-        
-        bool match1=false;
+
         if(s1.size()!=0 && s3[0]==s1[0])
-        {
-            match1=true;
-            string s1Tmp=s1.substr(1, s1.size()-1);
-            string s3Tmp=s3.substr(1, s3.size()-1);
-            isInterleave(s1Tmp, s2, s3Tmp, match);
-        }
+            isInterleaving(s1.substr(1), s2, s3.substr(1), match);
         if(s2.size()!=0 && s3[0]==s2[0])
-        {
-            match1=true;
-            string s2Tmp=s2.substr(1, s2.size()-1);
-            string s3Tmp=s3.substr(1, s3.size()-1);            
-            isInterleave(s1, s2Tmp, s3Tmp, match);
-        }
-        
-        if(match1==false)
-            return;	
-}
-*/
+            isInterleaving(s1, s2.substr(1), s3.substr(1), match);
+}*/
