7d511ca9b989fdf277a1abf729f8b961f4c4d42a
Guanglei Cao <caogl@umich.edu>
2015-01-24 16:08:32 -0500

    Update main.cpp

--- recursion/Number of connected regions/main.cpp
+++ recursion/Number of connected regions/main.cpp
@@ -1,9 +1,10 @@
+// Google interview question: http://www.careercup.com/question?id=5689704156495872
 /*
 0 0 1 1 1 1 0 0 
 0 0 0 0 1 0 0 0 
 0 0 0 0 0 0 0 0 
-0 0 0 0 0 0 1 1   ---> number of connected "ones": 5
-0 0 0 0 0 0 0 0 
+0 0 0 0 0 0 1 1   ---> (1)number of connected "ones": 5
+0 0 0 0 0 0 0 0   ---> (2)number of connected countries: 6
 0 0 1 0 1 1 1 0 
 0 0 0 1 0 0 0 0 
 1 1 0 0 0 0 0 1
@@ -15,7 +16,12 @@ using namespace std;
 
 int find_black_regions(vector<vector<bool> >& region);
 void make_white(vector<vector<bool> >& region, int i, int j, int rowNum, int colNum);
+
 vector<vector<bool> > makeTestCase(bool test[][8], int size1);
+
+int countries(vector<vector<bool> >& region);
+void help(vector<vector<bool> & region, vector<vector<bool> >& visited, int i, int j, int rowNum, int colNum, bool sign);
+
 int main()
 {
 	bool test1[][8]={{0,0,1,1,1,1,0,0},
@@ -32,6 +38,9 @@ int main()
 	}
 	int regionNum=find_black_regions(testCase);
 	cout<<"The number of connected black region is: "<<regionNum<<endl;
+	
+	testCase=makeTestCase(test1, 8);
+	cout<<"The number of countries is: "<<
 	return 0;
 }
 
@@ -87,3 +96,47 @@ void make_white(vector<vector<bool> >& region, int i, int j, int rowNum, int col
 	if(i<colNum-1 && j<rowNum-1)
 		make_white(region, i+1, j+1, rowNum, colNum);
 }
+
+int countries(vector<vector<bool> >& region)
+{
+	int rowNum=region.size();
+	int colNum=region[0].size();
+	vector<vector<bool> > visited(rowNum, vector<bool> (colNum, false));
+	int result=0;
+	
+	for(int i=0; i<rowNum; i++)
+	{
+		for(int j=0; j<colNum; j++)
+		{
+			if(!visited[i][j])	help(region, visited, i, j, rowNum, colNum, region[i][j]);
+			result++;
+		}
+	}
+	return result;
+}
+
+void help(vector<vector<bool> & region, vector<vector<bool> >& visited, int i, int j, int rowNum, int colNum, bool sign)
+{
+	if(!visited[i][j])	visited[i][j]=true;
+	else	return;
+	
+	if(i>0 && region[i-1][j]==sign)
+		help(region, visited, i-1, j, rowNum, colNum, sign);
+	if(i<colNum-1 && region[i+1][j]==sign)
+		help(region, visited, i+1, j, rowNum, colNum, sign);
+	if(j>0 && region[i][j-1]==sign)
+		help(region, visited, i, j-1, rowNum, colNum, sign);
+	if(j<rowNum-1 && region[i][j+1]==sign)
+		help(region, visited, i, j+1, rowNum, colNum, sign);
+	if(i>0 && j>0 && region[i-1][j-1]==sign)
+		help(region, visited, i-1, j-1, rowNum, colNum, sign);
+	if(i>0 && j<rowNum-1 && region[i-1][j+1]==sign)
+		help(region, visited, i-1, j+1, rowNum, colNum, sign);
+	if(i<colNum-1 && j>0 && region[i+1][j-1]==sign)
+		help(region, visited, i+1, j-1, rowNum, colNum, sign);
+	if(i<colNum-1 && j<rowNum-1 && region[i+1][j+1]==sign)
+		help(region, visited, i+1, j+1, rowNum, colNum, sign);
+}
+
+
+
