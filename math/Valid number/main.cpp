7eaf872f624c898750f6895c14004d6c9e03a156
Guanglei Cao <caogl@umich.edu>
2015-01-28 21:04:15 -0500

    Update main.cpp

--- math/Valid number/main.cpp
+++ math/Valid number/main.cpp
@@ -30,7 +30,8 @@ The integer and fractional parts contain only digits. For example, the number �
 and fractional part (64). Both of them are optional, but at least one of them must present. For example, 
 a single dot ‘.’ is not a valid number, but “1.”, “.1”, and “1.0” are all valid. Please note that “1.” is valid 
 because it implies “1.0”.
-*/
+
+A number could contain an optional exponent part, which is marked by a character ‘e’ followed by a whole number */
 
 #include<string>
 #include<cstring>
@@ -39,9 +40,7 @@ using namespace std;
 
 //reference : http://yucoding.blogspot.com/2013/05/leetcode-question-118-valid-number.html
 
-string deleteSpace(const char* s);
-bool isValid(string &s);
-bool isNumber(const char* s);
+bool isNumber(const char *s);
 
 int main()
 {
@@ -56,90 +55,47 @@ int main()
 	return 0;
 }
 
-// delete starting and trailing zeros
-string deleteSpace(const char* s)
+bool isNumber(const char *s) 
 {
-	if(strlen(s)==0)
-		return "";
-	int i=0;
-	while(s[i]==' ')
-		i++;
-	int j=strlen(s)-1;
-	while(s[j]==' ')
-		j--;
-	string result="";
-	for(int k=i; k<=j; k++)
-		result=result+s[k];
-	return result;
+        string ss=s;
+        // delete leading and trailing spaces
+        int i=0; 
+        while(ss[i]==' ')   i++;
+        int j=ss.size()-1;
+        while(ss[j]==' ')   j--;
+        ss=ss.substr(i, j-i+1);
+        
+        i=0;
+        bool isNumeric=false;
+        int n=ss.size();
+        if(i<n && (ss[i]=='+' || ss[i]=='-')) i++;
+        while(i<n && isdigit(ss[i]))
+        {
+            i++;
+            isNumeric=true;
+        }
+        if(i<n && ss[i]=='.')
+        {
+            i++;
+            while(i<n && isdigit(ss[i]))
+            {
+                i++;
+                isNumeric=true;
+            }
+        }
+        
+        // deal with the optional exponential part
+        if(isNumeric && i<n && ss[i]=='e')
+        {
+            i++;
+            isNumeric=false;
+            if(i<n && (ss[i]=='+' || ss[i]=='-'))   i++;
+            while(i<n && isdigit(ss[i]))
+            {
+                i++;
+                isNumeric=true;
+            }
+        }
+        
+        return isNumeric && (i==n);
 }
-
-bool isValid(string &s)
-{
-	int i=0;// position
-	// check if '.', 'e' or '0-9' exists before the current position
-	bool e=false;
-	bool dot=false;
-	bool digit=false;
-	
-	//for the fist char, the number can start with +, -, .
-	if(!isdigit(s[0]))
-	{
-		if(s[0]=='+' || s[0]=='-' || s[0]=='.')
-		{
-			if(s.size()==1)
-				return false;
-			if(s[0]=='.')
-				dot=true;
-		}
-		else
-			return false;
-	}
-	else
-		digit=true;
-	
-	for(int i=1; i<s.size()-1; i++)
-	{
-		switch (s[i])
-		{
-			case 'e':
-			case 'E': // e cannot occur follow +, -
-				if(e==false && s[i-1]!='+' && s[i-1]!='-' && digit==true && i!=s.size()-1)
-					e=true;
-				else
-					return false;
-				break;
-			case '+': // +, - can only occur after e
-			case '-':
-				if(s[i-1]!='e' && s[i-1]!='E')
-					return false;
-				break;
-			case '.':
-				if(dot==false && e==false)
-					dot=true;
-				else
-					return false;
-				break;
-			default:
-				if(isdigit(s[i]))
-					digit=true;
-				else
-					return false;
-				break;
-		}
-	}
-	
-	if(isdigit(s[s.size()-1]))
-		return true;
-	if(s[s.size()-1]=='.' && !dot && !e && isdigit(s[s.size()-2]))
-		return true;
-	return false;
-}
-
-bool isNumber(const char* s)
-{
-	string s1=deleteSpace(s);
-	if(s1.size()==0)
-		return false;
-	return isValid(s1);
-}
-
