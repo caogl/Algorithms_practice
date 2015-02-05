b8896e86801d320d996b8eb937256551e2fc08b7
Guanglei Cao <caogl@umich.edu>
2015-02-05 06:23:23 -0500

    Update main.cpp

--- math/Random maximum/main.cpp
+++ math/Random maximum/main.cpp
@@ -1,5 +1,11 @@
 // facebook interview question: http://www.1point3acres.com/bbs/forum.php?mod=viewthread&tid=114813&extra=page%3D1%26filter%3Dsortid%26sortid%3D311%26searchoption[3046][value][2]%3D2%26searchoption[3046][type]%3Dcheckbox%26sortid%3D311
 // reference: http://www.fgdsb.com/2015/01/15/random-maximum/
+/*
+给一个数组比如： ｛1，2，3，5，5，3，2，5｝
+请返回最大数的下标。
+如果最大数出现了好几次，就随机返回一个，随机是指，每个最大数的下标被选中的概率是一样的
+
+Naive的做法是先扫一遍，找出最大值和最大值的个数。然后从头再扫一遍即可。用Reservoir Sampling思路做，one pass就可以了。*/
 
 #include <stdlib.h>     /* srand, rand */
 #include <time.h>       /* time */
