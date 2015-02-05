2142bc982cd688924fa5c54b7a697ca3f5b7d1ff
Guanglei Cao <caogl@umich.edu>
2015-02-05 07:55:24 -0500

    Update main.cpp

--- dynamic programming/Number of palindrome substring/main.cpp
+++ dynamic programming/Number of palindrome substring/main.cpp
@@ -1,7 +1,9 @@
 // facebook interview questions: http://www.1point3acres.com/bbs/forum.php?mod=viewthread&tid=107664&extra=page%3D2%26filter%3Dsortid%26sortid%3D311%26searchoption[3046][value][2]%3D2%26searchoption[3046][type]%3Dcheckbox%26sortid%3D311
 
-/*
-actually the first part of the leetcode problem: "palindrome partition"  */
+/* string 有多少palindrome substring。
+   exp: 'aba' 返回4 , 'abba' 返回6
+   actually the first part of the leetcode problem: "palindrome partition"  
+   by using dynamic programming instead of brute force (see "Palindrome susbtring"), we reduced the runtime to O(n^2)*/
 
 #include<iostream>
 using namespace std;
