f2d59773297bb07dde961fc7ffdc949dbafd3774
Guanglei Cao <caogl@umich.edu>
2015-02-06 03:45:57 -0800

    Update main.cpp

--- math/Integer to Roman/main.cpp
+++ math/Integer to Roman/main.cpp
@@ -36,21 +36,20 @@ int main()
 	return 0;
 }
 
-string intToRoman(int num)
+string intToRoman(int num) 
 {
-	vector<int> values={1000,900,500,400,100,90,50,40,10,9,5,4,1};
-	vector<string> symbols={"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
-	string result;
-	int i=0;
-	while(num>0)
-	{
-		int k=num/values[i];
-		for(int j=0; j<k; j++)
-		{
-			result+=symbols[i];
-			num-=values[i];
-		}
-		i++;
-	}
-	return result;
+        vector<int> values={1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+        vector<string> symbols={"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+        int n=values.size();
+        string result;
+        for(int i=0; i<n; i++)
+        {
+            int k=num/values[i];
+            for(int j=0; j<k; j++)
+            {
+                result+=symbols[i];
+                num-=values[i];
+            }
+        }
+        return result;
 }
