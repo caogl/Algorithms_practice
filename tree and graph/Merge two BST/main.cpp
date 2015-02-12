1272a4f402822beec18e9ed75baa598254e73ec0
caogl <caogl@umich.edu>
2015-02-12 05:27:36 -0500

    update

---
+++ tree and graph/Merge two BST/main.cpp
@@ -0,0 +1,129 @@
+#include<iostream>
+using namespace std;
+
+struct TreeNode 
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+TreeNode* flattenBST(TreeNode* root); // the return node will be the leftmost node of original tree
+TreeNode* merge(TreeNode* root1, TreeNode* root2); // merge two flattened TreeNode
+TreeNode* listToBST(TreeNode* head);
+TreeNode* listToBST(TreeNode* begin, TreeNode* end);
+TreeNode* middle(TreeNode* begin, TreeNode* end); // get the middle node
+TreeNode* mergeBST(TreeNode* root1, TreeNode* root2); // merge two BST
+void inorderTraversal(TreeNode* root);
+
+int main()
+{
+	TreeNode* root1=new TreeNode(9);
+	root1->left=new TreeNode(5);
+	root1->left->left=new TreeNode(2);
+	root1->left->right=new TreeNode(7);
+	root1->right=new TreeNode(13);
+	root1->right->left=new TreeNode(11);
+	root1->right->right=new TreeNode(17);
+
+        TreeNode* root2=new TreeNode(8);
+        root2->left=new TreeNode(4);
+        root2->left->left=new TreeNode(3);
+        root2->left->right=new TreeNode(6);
+        root2->right=new TreeNode(12);
+        root2->right->left=new TreeNode(10);
+        root2->right->right=new TreeNode(14);
+
+	TreeNode* root=mergeBST(root1, root2);
+	inorderTraversal(root);
+	return 0;	
+}
+
+void inorderTraversal(TreeNode* root)
+{
+	if(root==nullptr)
+		return;
+	inorderTraversal(root->left);
+	cout<<root->val<<" ";
+	inorderTraversal(root->right);
+}
+
+TreeNode* mergeBST(TreeNode* root1, TreeNode* root2)
+{
+	if(!root1)	return root2;
+	if(!root2)	return root1;
+
+        TreeNode* root11=flattenBST(root1);
+        TreeNode* root22=flattenBST(root2);
+	TreeNode* root=merge(root11, root22);
+	return listToBST(merge(root11, root22));
+}
+
+TreeNode* middle(TreeNode* begin, TreeNode* end)
+{
+	TreeNode* fast=begin;
+	TreeNode* slow=begin;
+	while(fast!=end && fast->right!=end)
+	{
+		fast=fast->right->right;
+		slow=slow->right;
+	}	
+	return slow;
+}
+
+TreeNode* listToBST(TreeNode* begin, TreeNode* end)
+{
+	if(!begin || begin==end) // nothing to adjust
+		return begin;
+	if(begin->right=end) // the right pointer must be pointing to its parent
+	{
+		begin->left=nullptr;
+		begin->right=nullptr;
+		return begin;
+	}
+	
+	TreeNode* mid=middle(begin, end);
+	mid->left=listToBST(begin, mid);
+	mid->right=listToBST(mid->right, end);
+	return mid;
+}
+
+TreeNode* listToBST(TreeNode* head)
+{
+	if(!head)	return head;
+	listToBST(head, nullptr); 
+}
+
+TreeNode* merge(TreeNode* root1, TreeNode* root2)
+{
+	if(!root1)	return root2;
+	if(!root2)	return root1;
+
+	if(root1->val<root2->val)
+	{
+		root1->right=merge(root1->right, root2);
+		return root1;
+	}
+	else
+	{
+		root2->right=merge(root2->right, root1);
+		return root2;
+	}
+}
+
+TreeNode* flattenBST(TreeNode* root)
+{
+	if(!root) return nullptr;
+   
+    	TreeNode* leftTree = flattenBST(root->left);
+	TreeNode* rightTree = flattenBST(root->right);
+
+	TreeNode* head=leftTree; // the left most node of the left subtree
+	while(leftTree && leftTree->right)	
+		leftTree=leftTree->right; 
+    	if(leftTree) leftTree->right = root;
+    	root->left = leftTree;
+    	root->right = rightTree;
+    	return head? head:root;
+}
