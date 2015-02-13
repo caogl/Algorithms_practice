3ed0e398bbe8ffc6296e099223ca6fb1a5446a33
Guanglei Cao <caogl@umich.edu>
2015-02-13 00:54:28 -0800

    Update main.cpp

--- OOP design/Rank in stream/main.cpp
+++ OOP design/Rank in stream/main.cpp
@@ -1,9 +1,9 @@
 /* (1) Cracking the coding interview 5th edition 11.8
- * 	Imagine you are reading in a stream of integers. Periodically, you wish to be able to look up the rank of a number
- * 	x (the number of values less than or equal tox).
-	Implement the data structures and algorithms to support these operations.That is, implement the method track(int x),	    which is called when each number is generated, and the method getRankOfNumber(int x), which returns the number of 
-	values less than or equal to x (not including x itself).
-
+ * 	Imagine you are reading in a stream of integers. Periodically, you wish to be able to look up the rank of a 
+ *      number x (the number of values less than or equal tox).
+	Implement the data structures and algorithms to support these operations.That is, implement the method 
+	track(int x), which is called when each number is generated, and the method getRankOfNumber(int x), 
+	which returns the number of values less than or equal to x (not including x itself).
 	EXAMPLE:
 	Stream (in order of appearance): 5, 1, 4, 4, 5, 9, 7, 13, 3
 	getRankOfNumber(1) = 0
